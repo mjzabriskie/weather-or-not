f246e476e49ba95add4994f6ef87bfdb8501c411
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-25 17:49:08 -0600

    added localStorage saving and retrieval to keep track of searched cities.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,58 +5,79 @@ var currTemp = document.querySelector("#curr-temp");
 var currWind = document.querySelector("#curr-wind");
 var currHumidity = document.querySelector("#curr-humidity");
 var currUvi = document.querySelector("#curr-uvi");
-var placeArr = [];
+var savedPlaces = [];
 
-var getPlaceData = function(coord){
+var getPlaceData = function (coord) {
   var lat = coord.lat;
   var long = coord.long;
   var placeName = coord.placeName;
-  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly,alerts&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+long+"&exclude=minutely,hourly,alerts&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
 
-  fetch(apiUrl).then(function(response){
-      if(response.ok){
-        response.json().then(function(data){
-          currentCity.textContent = placeName;
-          currTemp.textContent = data.current.temp;
-          cityNameEl.value = "";
-        })
-      } else {
-        alert("Something went wrong, please try again!");
-      }
-  })
-}
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        currentCity.textContent = placeName;
+        currTemp.textContent = data.current.temp;
+        cityNameEl.value = "";
+      });
+    } else {
+      alert("Something went wrong, please try again!");
+    }
+  });
+};
 
-var getPlace = function(){
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameEl.value+"&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
-  fetch(apiUrl).then(function(response){
-      if(response.ok){
-          response.json().then(function(data) {
-            var lat = data.coord.lat;
-            var long = data.coord.lon;
-            var placeName = data.name;
-            var placeObj = {placeName, lat, long};
-            placeArr.push(placeObj);
-            console.log(placeArr);
-          getPlaceData(placeObj);
-          });
-      } else {
-        alert("Enter a valid city!");
-      }
-  })
-}
+var createHistoryBtn = function (placeName) {};
 
-var savePlace = function(){
+var loadPlaces = function () {
+  savedPlaces = localStorage.getItem("places");
+  if (!savedPlaces) {
+    return false;
+  }
 
-}
+  savedPlaces = JSON.parse(savedPlaces);
+  console.log(savedPlaces);
 
+  //   for (var i = 0; i < savedTasks.length; i++) {
+  //     createTaskEl(savedTasks[i]);
+  // }
+};
 
+var savePlace = function (obj) {
+  if (!savedPlaces) {
+    savedPlaces = [];
+  }
+  savedPlaces.push(obj);
+  localStorage.setItem("places", JSON.stringify(savedPlaces));
+};
 
-submitBtnEl.addEventListener("click", function(event){
+var getPlace = function () {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cityNameEl.value + "&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var lat = data.coord.lat;
+        var long = data.coord.lon;
+        var placeName = data.name;
+        var placeObj = { placeName, lat, long };
+        savePlace(placeObj);
+        getPlaceData(placeObj);
+      });
+    } else {
+      alert("Enter a valid city!");
+    }
+  });
+};
+
+loadPlaces();
+
+submitBtnEl.addEventListener("click", function (event) {
   event.preventDefault();
   getPlace();
 
   //getPlaceData(coord);
-})
+});
 
 /*
   fetch(apiUrl)
@@ -97,12 +118,3 @@ submitBtnEl.addEventListener("click", function(event){
 //     });
 //     autocomplete.addEventListener("place_changed", onPlaceChanged);
 // }
-
-
-
-
-
-
-var getWeatherData = function(){
-    var apiUrl
-};
