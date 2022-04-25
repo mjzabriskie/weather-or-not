9e90ca83639ccf53e16363ceb86111c73a8e6935
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-25 14:13:45 -0600

    Created two new functions, one to get the name and coordinates of the entered location, and another to use the coordinates obtained from the first to get local weather data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,57 @@ var currTemp = document.querySelector("#curr-temp");
 var currWind = document.querySelector("#curr-wind");
 var currHumidity = document.querySelector("#curr-humidity");
 var currUvi = document.querySelector("#curr-uvi");
+var placeArr = [];
+
+var getPlaceData = function(coord){
+  var lat = coord.lat;
+  var long = coord.long;
+  var placeName = coord.placeName;
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly,alerts&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
+
+  fetch(apiUrl).then(function(response){
+      if(response.ok){
+        response.json().then(function(data){
+          currentCity.textContent = placeName;
+          currTemp.textContent = data.current.temp;
+          cityNameEl.value = "";
+        })
+      } else {
+        alert("Something went wrong, please try again!");
+      }
+  })
+}
+
+var getPlace = function(){
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameEl.value+"&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
+  fetch(apiUrl).then(function(response){
+      if(response.ok){
+          response.json().then(function(data) {
+            var lat = data.coord.lat;
+            var long = data.coord.lon;
+            var placeName = data.name;
+            var placeObj = {placeName, lat, long};
+            placeArr.push(placeObj);
+            console.log(placeArr);
+          getPlaceData(placeObj);
+          });
+      } else {
+        alert("Enter a valid city!");
+      }
+  })
+}
+
+var savePlace = function(){
+
+}
+
+
 
 submitBtnEl.addEventListener("click", function(event){
-    event.preventDefault();
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameEl.value+"&units=imperial&appid=ffd54ea96bf3b94acf8e72a75c1d3667";
-    fetch(apiUrl).then(function(response){
-        if(response.ok){
-            response.json().then(function(data) {
-            console.log(data.coord);
-            currentCity.textContent = data.name;
-            currTemp.textContent = data.main.temp;
-            });
-        }
-    })
+  event.preventDefault();
+  getPlace();
 
+  //getPlaceData(coord);
 })
 
 /*
